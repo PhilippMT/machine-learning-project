f686e4cfbb5b4e6b5d466e7ca7b39bcfb06b7433
= <philipp.maricek@tui.com>
2021-08-29 14:06:30 +0200

    Upload

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=philippmt/ml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml \
+    --image=$dockerpath \
+    --port=80 --labels app=ml \
+    -n default
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods -n default
 
 # Step 4:
 # Forward the container port to a host
+echo "Sleep for few seconds to wait for pod to be ready"
+sleep 10
 
+kubectl port-forward ml 8000:80 -n default
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=philippmt/ml
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker image tag ml $dockerpath
+docker login --username philippmt
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
